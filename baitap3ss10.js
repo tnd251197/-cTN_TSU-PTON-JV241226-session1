16cec1200e796e12ec162e0bd049ab9d4ce5edf5
tnd251197 <widter1234@gmail.com>
2025-02-23 18:50:15 +0700

    baitap3ss10

---
+++ baitap3ss10.js
@@ -0,0 +1,7 @@
+// khai báo biến width và height
+let width = 20;
+let height = 5;
+// tính diện tính hình chữ nhật
+let area = width*height;
+// in kết quả ra màn hình 
+document.write("Diện tích hình chữ nhật"+area);
