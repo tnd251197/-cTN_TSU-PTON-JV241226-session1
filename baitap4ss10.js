80681d64cc80649748c4b56e27749ff3a33e3549
tnd251197 <widter1234@gmail.com>
2025-03-05 04:10:32 +0700

    baitap4ss10js

---
+++ baitap4ss10.js
@@ -0,0 +1,12 @@
+// câu chào
+let cauChao = "wellcome to Rikkei Academy";
+console.log(cauChao);
+let quyetTam = "Quyết tâm học lập trình javascript";
+document.write (quyetTam);
+alert ("chào mừng bạn đến với học viện Rikkei Academy");
+let cauHoi = confirm("bạn đủ 18 tuổi chưa!");
+if (cauHoi){
+    console.log ("bạn đủ 18 tuổi.");
+}else{
+    console.log("bạn chưa đủ 18 tuổi hoặc đã chọn No.");
+}
