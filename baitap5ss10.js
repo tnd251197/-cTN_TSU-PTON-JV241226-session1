183e854796717bec891ac39f00c0f796b6a68faf
tnd251197 <widter1234@gmail.com>
2025-03-05 04:25:36 +0700

    baitap5ss10.js

---
+++ baitap5ss10.js
@@ -0,0 +1,7 @@
+let firstNumber = prompt("nhập số từ bàn phím");
+let secondNumber = prompt ("nhập số từ bàn phím");
+if (firstNumber%secondNumber===0){
+    alert("firstNumber là bội số của secondNumber")
+}else {
+    alert ("firstNumber không là bội số của secondNumber")
+};
