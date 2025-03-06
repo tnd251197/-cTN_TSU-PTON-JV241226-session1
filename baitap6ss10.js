e3a16bfa3dff6d9b7c31c4ee68e08bfe0faefe35
tnd251197 <widter1234@gmail.com>
2025-03-07 02:34:04 +0700

    baitap6ss10.js

---
+++ baitap6ss10.js
@@ -0,0 +1,7 @@
+// khai báo điểm 3 môn học 
+let math = 8
+let physics = 7
+let chemistry = 9
+// tính điểm trung bình 
+let avg = (math + physics + chemistry)/3;
+console.log ( "điểm trung bình=",avg );
