cfe4e956011eeaf6f799234b1dab905a2a79d8b6
tnd251197 <widter1234@gmail.com>
2025-03-10 04:57:58 +0700

    baitap1ss13.js

---
+++ baitap1ss13.js
@@ -0,0 +1,18 @@
+// thực hiện mãng
+let listTuvung = [
+    "haha",
+    "chichi",
+    "imoto",
+    "ane",
+    "ani",
+    "chonan",
+    "shimai",
+    "suekko",
+    "mogo",
+];
+//  console.log(listTuvung);
+// thêm phân tử vào mảng đầu và cuối
+listTuvung.push("oya");
+console.log(listTuvung);
+listTuvung.unshift("obasan");
+console.log(listTuvung);
