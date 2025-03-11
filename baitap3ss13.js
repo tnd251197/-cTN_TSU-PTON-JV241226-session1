9b51b9e8163a9b49213d8840866266dac14aba43
tnd251197 <widter1234@gmail.com>
2025-03-11 23:34:57 +0700

    baitap3ss13.js

---
+++ baitap3ss13.js
@@ -0,0 +1,9 @@
+// nhập vào dẩy số bất kỳ
+let numberInput = prompt ("nhập vào dãy số bất kỳ");
+    let numberArry = numberInput.split(",");
+    let numberreverse = numberArry.reverse ();
+    alert ("mảng sau khi đảo ngược:" + numberreverse);
+         
+
+
+
